dccd457c3c7c7e7a4a6314ecc7f864694d24f786
aviyasha <aviya@joinreal.com>
2018-06-04 12:31:23 +0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -24,9 +23,8 @@ ActiveRecord::Schema.define(version: 20180601093032) do
     t.string   "resource_type"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.index ["resource_type", "resource_id"], name: "index_addresses_on_resource_type_and_resource_id"
   end
-
-  add_index "addresses", ["resource_type", "resource_id"], name: "index_addresses_on_resource_type_and_resource_id"
 
   create_table "opportunities", force: :cascade do |t|
     t.string   "title"
@@ -67,9 +65,8 @@ ActiveRecord::Schema.define(version: 20180601093032) do
     t.string   "resume_content_type"
     t.integer  "resume_file_size"
     t.datetime "resume_updated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
